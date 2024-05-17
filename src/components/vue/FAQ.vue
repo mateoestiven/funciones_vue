d5f80d21a335706798929f15ceeab6b3c799430f
mateoestiven <pallaresjacomem@gmail.com>
2024-05-17 17:41:10 -0500

    componente acordeon

--- src/components/vue/FAQ.vue
+++ src/components/vue/FAQ.vue
@@ -1,93 +1,41 @@
 <script setup>
 import { ref } from 'vue'
 
-const showTheme1 = ref(false)
-const showTheme2 = ref(false)
-const showTheme3 = ref(false)
-const showTheme4 = ref(false)
+const data = ['Tema1','Tema2','Tema3','Tema4']
 
+const selectIndex = ref(0)
 
-
-
-function showInfo(index){
-    if(index == 1){
-        showTheme1.value = true
-        showTheme2.value = false
-        showTheme3.value = false
-        showTheme4.value = false
-    }else if(index == 2){
-        showTheme1.value = false
-        showTheme2.value = true
-        showTheme3.value = false
-        showTheme4.value = false
-    }else if(index == 3){
-        showTheme1.value = false
-        showTheme2.value = false
-        showTheme3.value = true
-        showTheme4.value = false
-    }else if(index == 4){
-        showTheme1.value = false
-        showTheme2.value = false
-        showTheme3.value = false
-        showTheme4.value = true
+function showInfo(i){
+    if(i == selectIndex.value){
+        selectIndex.value = -1
+    }else{
+        selectIndex.value = i
     }
     
 }
 </script>
 
 <template>
-    <div class="bg-blue-700 p-2 max-w-60 mx-auto mt-12">
-        <div>
-            <h1 @click="showInfo(1)"
-            class="select-none hover:cursor-pointer bg-blue-400 text-white px-1 flex justify-between">
-                Tema 1
+    <div class="bg-blue-600 p-2 max-w-60 mx-auto mt-12">
+        <div
+        v-for="(theme, i) in data"
+        :key="i"
+        >
+
+            <h1 @click="showInfo(i)"
+            class="select-none hover:cursor-pointer bg-blue-400 text-white px-1 flex justify-between"
+            :class="{'bg-blue-800': selectIndex == i}"
+            >
+               {{ theme }} 
                 <span>
-                    {{ showTheme1 ? '-' : '+' }}
+                    {{ selectIndex == i ? '-' : '+' }}
                 </span>
             </h1>
-            <div v-show="showTheme1"
+            <div v-show="selectIndex == i"
             class="bg-blue-50/50 p-1">
                 Texto del tema 1
             </div>
         </div>
-        <div>
-            <h1 @click="showInfo(2)"
-            class="select-none hover:cursor-pointer bg-blue-400 text-white px-1 flex justify-between">
-                Tema 2
-                <span>
-                    {{ showTheme2 ? '-' : '+' }}
-                </span>
-            </h1>
-            <div v-show="showTheme2"
-            class="bg-blue-50/50 p-1">
-                Texto del tema 2
-            </div>
-        </div>
-        <div>
-            <h1 @click="showInfo(3)"
-            class="select-none hover:cursor-pointer bg-blue-400 text-white px-1 flex justify-between">
-                Tema 3
-                <span>
-                    {{ showTheme3 ? '-' : '+' }}
-                </span>
-            </h1>
-            <div v-show="showTheme3"
-            class="bg-blue-50/50 p-1">
-                Texto del tema 3
-            </div>
-        </div>
-        <div>
-            <h1 @click="showInfo(4)"
-            class="select-none hover:cursor-pointer bg-blue-400 text-white px-1 flex justify-between">
-                Tema 4
-                <span>
-                    {{ showTheme4 ? '-' : '+' }}
-                </span>
-            </h1>
-            <div v-show="showTheme4"
-            class="bg-blue-50/50 p-1">
-                Texto del tema 4
-            </div>
-        </div>
+       
     </div>
 </template>
