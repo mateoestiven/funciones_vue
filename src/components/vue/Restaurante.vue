50f7ca25de914ccd14086c958320a63dab729356
mateoestiven <pallaresjacomem@gmail.com>
2024-05-24 09:56:49 -0500

    practica Restaurante

---
+++ src/components/vue/Restaurante.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+const dias = [
+    {
+        dia: "lunes",
+        menu: {
+            desayuno: ["joan", "joan_2", "joan_3"],
+            almuerzo: ["joan", "joan_2", "joan_3"],
+            cena: ["joan", "joan_2", "joan_3"]
+        }
+    },
+    {dia: "martes",
+        menu: {
+            desayuno: ["joan", "joan_2", "joan_3"],
+            almuerzo: ["joan", "joan_2", "joan_3"],
+            cena: ["joan", "joan_2", "joan_3"]
+        }
+    },
+    {dia: "miercoles",
+        menu: {
+            desayuno: ["joan", "joan_2", "joan_3"],
+            almuerzo: ["joan", "joan_2", "joan_3"],
+            cena: ["joan", "joan_2", "joan_3"]
+        }
+    },
+    {dia: "jueves",
+        menu: {
+            desayuno: ["joan", "joan_2", "joan_3"],
+            almuerzo: ["joan", "joan_2", "joan_3"],
+            cena: ["joan", "joan_2", "joan_3"]
+        }
+    },
+    {dia: "viernes",
+        menu: {
+            desayuno: ["joan", "joan_2", "joan_3"],
+            almuerzo: ["joan", "joan_2", "joan_3"],
+            cena: ["joan", "joan_2", "joan_3"]
+        }
+    },
+    {dia: "sabado",
+        menu: {
+            desayuno: ["joan", "joan_2", "joan_3"],
+            almuerzo: ["joan", "joan_2", "joan_3"],
+            cena: ["joan", "joan_2", "joan_3"]
+        }
+    }
+]
+
+
+</script>
+
+<template>
+
+</template>
